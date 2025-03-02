5224ab6714301e7c45e9490f47f7468a8b9ff10e
ghostthe935@gmail.com <rat@Zakaria>
2025-03-02 07:35:22 +0000

    The start

---
+++ build/CMakeFiles/detox.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/detox.dir/src/main.c.o"
+  "CMakeFiles/detox.dir/src/main.c.o.d"
+  "detox"
+  "detox.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/detox.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+#define N1 "###################################\n###########################"
+
+
+int main(int argc, char *argv[]){
+	
+	printf("%s\n", N1);
+	return 0;
+}
