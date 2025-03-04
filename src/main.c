6e1cbf32a4d920ac49d9ce64f89b2ead2ee98ec4
Zakaria742 <zaakizakaria12@gmail.com>
2025-03-04 10:48:39 +0000

    Counter finished

--- src/main.c
+++ src/main.c
@@ -3,25 +3,26 @@
 #include <unistd.h>
 #include <math.h>
 #include <stdbool.h>
+#include <string.h>
+#include <termios.h>
 
-#define N0 "#########\n#       #\n#       #\n#       #\n#########\n"
-#define N1 "\t#\n   #\t#\n#\t#\n\t#\n   ##########\n"
-#define N2 "#########\n\t#\n#########\n#\n#########\n"
-#define N3 "#########\n\t#\n#########\n\t#\n#########\n"
-#define N4 "4\n"
-#define N5 "5\n"
-#define N6 "6\n"
-#define N7 "7\n"
-#define N8 "8\n"
-#define N9 "9\n"
+
+#define N0 "#######\n#     #\n#     #\n#     #\n#######"
+#define N1 "   #\n#  #\n   #\n   #\n#######"
+#define N2 "#######\n      #\n#######\n#\n#######"
+#define N3 "#######\n      #\n#######\n      #\n#######"
+#define N4 "4"
+#define N5 "5"
+#define N6 "6"
+#define N7 "7"
+#define N8 "8"
+#define N9 "9\t"
 #define P "####\n####\n####\n####"
 
 char *print_number(int time, int iterations)
 {
 	unsigned digit = 0;
 	digit = (time / iterations);
-	if (!digit && iterations == 10)
-		return "end";
 	unsigned short res = (unsigned short)round(digit) % 10;
 	switch (res)
 	{
@@ -70,43 +71,77 @@ int num_digits(int number)
 		number /= 10;
 	} while (number != 0);
 
-		return count;
+	return count;
+}
+
+char **get_array(int t)
+{
+	int time = t;
+	char **array = (char **)calloc(1, sizeof(char *));
+	for (size_t i = 0; i < num_digits(time); i++)
+	{
+		array[i] = (char *)calloc(num_digits(time), sizeof(char));
+	}
+
+	system("clear");
+	int j = num_digits(time);
+	int k = 0;
+	while (1)
+	{
+		int power = pow(10, j - 1);
+		if (j == 0)
+			break;
+		array[k] = print_number(time, power);
+		j--;
+		k++;
+	}
+	return array;
+}
+
+struct termios origin;
+void disable_raw(){
+	tcgetattr(STDIN_FILENO, &origin);
+	tcsetattr(STDOUT_FILENO, TCSAFLUSH, &origin);
+}
+
+void raw(){
+	struct termios raw = origin;
+	atexit(disable_raw);
+	tcgetattr(STDIN_FILENO, &raw);
+	raw.c_iflag &= ~( IXOFF | BRKINT | ICRNL );
+	raw.c_oflag &= ~(OPOST);
+	raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
+	tcsetattr(STDOUT_FILENO, TCSAFLUSH, &raw);
 }
 
 int main(int argc, char *argv[])
 {
-
-	if (argc < 2)
-	{
-		fprintf(stderr, "No time has been specified\nUse : detox <time in seconds>\n");
-		exit(-1);
-	}
+	raw();
+	//Waiting for input from the user;
+	// if (argc < 2)
+	// {
+	// 	fprintf(stderr, "No time has been specified\nUse : detox <time in seconds>\n");
+	// 	exit(-1);
+	// }
 	// printf("%s\n", print_number(argv));
-	int time = atoi(argv[1]);
-	bool Ended = false;
-	while (!Ended)
+	int time = 121;
+	while (time)
 	{
-
-		for (size_t i = 0; i < atoi(argv[1]); i++)
+		char **char_list = get_array(time);
+		printf("DETOX TIMER");
+		for (size_t i = 0; i < num_digits(time); i++)
 		{
-			system("clear");
-			int j = num_digits(time);
-			while (1)
-			{
-				int power = pow(10, j);
-				if (print_number(time, power) == "end")
-					break;
-				printf("%s", print_number(time, power));
-				j--;
-				usleep(1 * 1000000);
-			}
-			time--;
-			if (time == -1)
-			{
-				Ended = true;
-				break;
-			}
+			printf("\r\n%s", char_list[i]);
 		}
+		printf("\r\n");
+		usleep(1 * 1000000);
+		time--;
 	}
+
+	// for(size_t i = 0; i < num_digits(atoi(argv[1])); i++){
+	// 	free(char_list[i]);
+	// }
+	// free(char_list);
+
 	return 0;
 }
